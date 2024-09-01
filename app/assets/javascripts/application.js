be724d79cee0683b581d4ab9a1b68cbd3f8fe273
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-09-02 00:23:35 +0100

    add high charts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,13 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
+import { Chart } from "highcharts"
+
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+  new Chart("test-chart", { series: [{ data: [1, 2, 3] }] })
+
+  console.log("Hello world!")
 })
+
+alert()
