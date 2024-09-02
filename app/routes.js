9153c223a404d81320290c2b4b1bdb32abb9c639
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-09-02 02:12:37 +0100

    put fetch in try catch

--- app/routes.js
+++ app/routes.js
@@ -8,19 +8,19 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 router.get("/datasette/performance.json", async (req, res) => {
-  const response = await fetch(
-    `https://datasette.planning.data.gov.uk/performance.json?sql=${encodeURIComponent(
-      req.query.sql,
-    )}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    },
-  )
-
   try {
+    const response = await fetch(
+      `https://datasette.planning.data.gov.uk/performance.json?sql=${encodeURIComponent(
+        req.query.sql,
+      )}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      },
+    )
+
     res.json(await response.json())
   } catch (e) {
     res.status(500).json({ error: e.message })
