c63f95476a26981e27bc848de765dc2046faf09a
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-09-02 02:02:30 +0100

    show chart for top orgs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,13 +3,44 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-import { Chart } from "highcharts"
-
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
-  new Chart("test-chart", { series: [{ data: [1, 2, 3] }] })
-
-  console.log("Hello world!")
 })
 
-alert()
+document.addEventListener("DOMContentLoaded", async function () {
+  let data = await fetch(
+    "/datasette/performance.json?sql=select+%22name%22%2C+sum%28active_endpoint_count%29+as+%22active_endpoints%22+from+provision_summary%0D%0Agroup+by+organisation%0D%0Ahaving+active_endpoints+%3E+0%0D%0Aorder+by+active_endpoints+desc+limit+10",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    },
+  )
+
+  data = await data.json()
+
+  const chart = Highcharts.chart("container", {
+    chart: {
+      type: "bar",
+    },
+    title: {
+      text: "Top 10 organisations by active endpoints",
+    },
+    xAxis: {
+      categories: data.rows.map((row) => row[0]),
+    },
+    yAxis: {
+      title: {
+        text: "Active endpoints",
+      },
+    },
+    series: [
+      {
+        name: "Active endpoints",
+        data: data.rows.map((row) => row[1]),
+        color: "#1d70b8",
+      },
+    ],
+  })
+})
--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,22 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
+const govukPrototypeKit = require("govuk-prototype-kit")
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.get("/datasette/performance.json", async (req, res) => {
+  const response = await fetch(
+    `https://datasette.planning.data.gov.uk/performance.json?sql=${encodeURIComponent(
+      req.query.sql,
+    )}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    },
+  )
+
+  res.json(await response.json())
+})
