2d570d7929c41c63ae1f7826c09d050bfd1794af
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-10-07 16:06:05 +0100

    add data needed for stats

--- app/services/PerformanceService.js
+++ app/services/PerformanceService.js
@@ -22,7 +22,48 @@ const getStats = async function () {
   const datasets = await getDatasetCount()
 
   return {
-    dataset_count: datasets.rows.length,
+    datasets: {
+      count: datasets.rows.length,
+      article_4_direction_coverage_percentage: null,
+      article_4_direction_coverage_percentage_increase_last_30_days: null,
+      article_4_direction_area_coverage_percentage: null,
+      article_4_direction_area_coverage_percentage_increase_last_30_days: null,
+      conservation_area_coverage_percentage: null,
+      conservation_area_coverage_percentage_increase_last_30_days: null,
+      conservation_area_document_coverage_percentage: null,
+      conservation_area_document_coverage_percentage_increase_last_30_days:
+        null,
+      listed_building_coverage_percentage: null,
+      listed_building_coverage_percentage_increase_last_30_days: null,
+      tree_preservation_order_coverage_percentage: null,
+      tree_preservation_order_coverage_percentage_increase_last_30_days: null,
+      tree_preservation_zone_coverage_percentage: null,
+      tree_preservation_zone_coverage_percentage_increase_last_30_days: null,
+      tree_coverage_percentage: null,
+      tree_coverage_percentage_increase_last_30_days: null,
+    },
+    organisation: {
+      count: null,
+    },
+    data_sources: {
+      total: null,
+      total_provided: null,
+      increase_last_30_days: null,
+      increase_last_30_days_percentage: null,
+    },
+    local_planning_authorities: {
+      total: null,
+      count_with_data_sources: null,
+      count_with_no_data_sources: null,
+      increase_last_30_days: null,
+      increase_last_30_days_percentage: null,
+    },
+    quality_checks: {
+      total_data_sources_with_no_issues: null,
+      total_data_sources_with_specification_conformation: null,
+      total_improved_data_sources_in_last_30_days: null,
+      total_improved_data_sources_in_last_30_days_percentage: null,
+    },
   }
 }
 
