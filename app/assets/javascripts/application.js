589c0e882c8e28bc9ca2b452b1a70f6b3045bf56
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-10-08 12:57:50 +0100

    add marking for all hardcoded data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@ window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
 
-document.addEventListener("DOMContentLoaded", async function () {
+async function addActiveEndpointChart() {
   let data = await fetch(
     `/datasette/performance.json?sql=select+organisation_name+as+"name"%2C+sum%28active_endpoint_count%29+as+"active_endpoints"+from+provision_summary%0D%0Awhere+organisation+like+"local-authority%3A%25"%0D%0Agroup+by+organisation%0D%0Ahaving+active_endpoints+>+0%0D%0Aorder+by+active_endpoints+desc+limit+10`,
     {
@@ -43,4 +43,12 @@ document.addEventListener("DOMContentLoaded", async function () {
       },
     ],
   })
+}
+
+async function addDatasetEntryByMonthChart() {
+  // dataset.json?sql=SELECT%0D%0A++strftime%28%27%25Y-%25m%27%2C+entry_date%29+AS+year_month%2C%0D%0A++COUNT%28*%29+AS+dataset_count%0D%0AFROM%0D%0A++dataset%0D%0AGROUP+BY%0D%0A++year_month%0D%0AORDER+BY%0D%0A++year_month%3B
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  addActiveEndpointChart()
 })
